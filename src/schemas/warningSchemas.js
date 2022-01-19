029c501a70d1692c8155b9dfb770b2f99fe3bee1
kiss8981 <kdh89811@gmail.com>
2022-01-20 03:20:42 +0900

    fix: package name

--- src/schemas/warningSchemas.js
+++ src/schemas/warningSchemas.js
@@ -1,25 +1,13 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require('mongoose')
 
-const warningSchema = mongoose.Schema({
-    userId: {
-        type: String,
-        default: ""
-    },
-    guildId: {
-        type: String,
-        default: ""
-    },
-    reason: {
-        type: String,
-        default: ""
-    },
-    managerId: {
-        type: String,
-        default: ""
-    },
-    published_date: { type: Date, default: Date.now }
-}, {collection: 'warning'});
+const warningSchema = Schema({
+  userId: { type: String, default: '' },
+  guildId: { type: String, default: '' },
+  reason: { type: String, default: '' },
+  managerId: { type: String, default: '' },
+  published_date: { type: Date, default: Date.now }
+}, {collection: 'warning'})
 
-const Warning = mongoose.model('warning', warningSchema);
+const Warning = model('warning', warningSchema, 'warning')
 
 module.exports = { Warning }
