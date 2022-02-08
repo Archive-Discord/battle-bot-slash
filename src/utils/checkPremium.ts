3fe067524c648e24ade2043682056c29fbdf6602
kiss8981 <kdh89811@gmail.com>
2022-02-08 21:37:43 +0900

    fix: premium handler verify

--- src/utils/checkPremium.ts
+++ src/utils/checkPremium.ts
@@ -3,12 +3,12 @@ import Premium from "../schemas/premiumSchemas";
 import BotClient from "../structures/BotClient";
 
 const checkPremium = async(client: BotClient, guild: Guild) => {
-  let premium = await Premium.findOne({guild_id: guild.id})
+  const premium = await Premium.findOne({guild_id: guild.id})
   if(!premium){
     return false;
   } else {
-    let now = new Date()
-    let premiumDate = new Date(premium.nextpay_date)
+    const now = new Date()
+    const premiumDate = new Date(premium.nextpay_date)
     if(now < premiumDate) {
       return true
     } else {
