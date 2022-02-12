3646bdf6d8b14892f10776f672b7b40619981f39
kiss8981 <kdh89811@gmail.com>
2022-02-13 06:48:43 +0900

    feat: melon chart play

--- src/server/router/back.ts
+++ src/server/router/back.ts
@@ -7,9 +7,14 @@ app.post('/', async(req: any, res)=> {
   const guild = req.guild as Guild
   const client = req.client as BotClient
   let queue = client.player.getQueue(guild.id)
-  if(!queue) return res.status(200).json({ status: 200, message: "재생중인 노래가 없습니다"})
-  await queue.back()
-  return res.status(200).json({ status: 200, message: "노래를 스킵했어요!"});
+  if(!queue) return res.status(404).json({ status: 404, message: "재생중인 노래가 없습니다"})
+  await queue.back().catch((e)=> {
+    if(e) {
+      if(e.statusCode === "TrackNotFound") return res.status(404).json({ status: 404, message: "이전 노래를 찾을 수 없어요!"});
+      else return res.status(500).json({ status: 500, message: "알 수 없는 오류가 발생했어요!", data: e.statusCode});
+    }
+  })
+  return res.status(200).json({ status: 200, message: "이전 노래를 재생했어요!"});
 })
 
 
--- src/server/router/pause.ts
+++ src/server/router/pause.ts
@@ -7,7 +7,7 @@ app.post('/', async(req: any, res)=> {
   const guild = req.guild as Guild
   const client = req.client as BotClient
   let queue = client.player.getQueue(guild.id)
-  if(!queue) return res.status(200).json({ status: 200, message: "재생중인 노래가 없습니다"})
+  if(!queue) return res.status(400).json({ status: 400, message: "재생중인 노래가 없습니다"})
   if (queue.connection.paused) {
     queue.setPaused(false);
     return res.status(200).json({ status: 200, message: "일시정지를 해제 했어요!"});
