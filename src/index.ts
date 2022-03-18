ced68ff7abf4703cec4330049b7ec3b66a8719a2
Dohyun Kim <35857297+kiss8981@users.noreply.github.com>
2022-03-19 00:05:12 +0900

    Revert "fix: musicapi"

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,8 @@ import config from '../config'
 import chalk from 'chalk'
 import { name } from '../package.json'
 import Logger from './utils/Logger'
-import web from './server'
-import { client } from './bot'
 
 const logger = new Logger('shard')
-const loggerWeb = new Logger('web')
 
 console.log(
   chalk.cyanBright(`
@@ -33,10 +30,4 @@ if (!config.bot.sharding) {
   manager.on('shardCreate', async (shard) => {
     logger.info(`Shard #${shard.id} created.`)
   })
-}
-
-try {
-  web(client)
-} catch(e) {
-  loggerWeb.error(e as string)
 }
