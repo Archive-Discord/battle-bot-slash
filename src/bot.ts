a602395fa52bd3cd0473a457e0fc68988cf7a177
kiss8981 <kdh89811@gmail.com>
2022-03-19 00:15:01 +0900

    fix: musicapi

--- src/bot.ts
+++ src/bot.ts
@@ -6,7 +6,6 @@ import CommandManager from './managers/CommandManager'
 import EventManager from './managers/EventManager'
 import DatabaseManager from './managers/DatabaseManager'
 import ButtonManager from './managers/ButtonManager'
-import web from './server'
 
 const logger = new Logger('main')
 
@@ -27,4 +26,3 @@ button.load(path.join(__dirname, 'buttons'))
 database.load()
 
 client.start(config.bot.token)
-web(client)
