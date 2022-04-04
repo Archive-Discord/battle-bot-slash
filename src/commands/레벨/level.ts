0833c4d280da1699ec7db9914f27737a977cc194
kiss8981 <kdh89811@gmail.com>
2022-04-05 00:31:40 +0900

    fix: type

--- src/commands/레벨/level.ts
+++ src/commands/레벨/level.ts
@@ -43,12 +43,12 @@ export default new BaseCommand(
         if(!levelDB) {
           successEmbed.setTitle(`${interaction.user.username}님의 레벨 정보`)
           successEmbed.setDescription(`현제 \`LV.0\`입니다. 다음 레벨까지 \`0XP / 15XP\` 남았습니다.\n
-          [여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨이 30% 부스터를 사용할 수 있어요! (준비중)`)
+          [여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨 30% 부스터를 사용할 수 있어요! (준비중)`)
           return interaction.editReply({embeds: [successEmbed]})
         } else {
           successEmbed.setTitle(`${interaction.user.username}님의 레벨 정보`)
           successEmbed.setDescription(`현제 \`LV.${levelDB.level ? levelDB.level : 0}\`입니다. 다음 레벨까지 \`${levelDB.currentXP}XP / ${(!levelDB.level ? 1 : levelDB.level + 1) * 13}XP\` 남았습니다.\n
-          [여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨이 30% 부스터를 사용할 수 있어요! (준비중)`)
+          [여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨 30% 부스터를 사용할 수 있어요! (준비중)`)
           return interaction.editReply({embeds: [successEmbed]})
         }
       } else {
@@ -56,12 +56,12 @@ export default new BaseCommand(
         if(!levelDB) {
           successEmbed.setTitle(`${user.username}님의 레벨 정보`)
           successEmbed.setDescription(`현제 \`LV.0\`입니다. 다음 레벨까지 \`0XP / 15XP\` 남았습니다.\n
-          [여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨이 30% 부스터를 사용할 수 있어요! (준비중)`)
+          [여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨 30% 부스터를 사용할 수 있어요! (준비중)`)
           return interaction.editReply({embeds: [successEmbed]})
         } else {
           successEmbed.setTitle(`${user.username}님의 레벨 정보`)
           successEmbed.setDescription(`현제 \`LV.${levelDB.level ? levelDB.level : 0}\`입니다. 다음 레벨까지 \`${levelDB.currentXP}XP / ${(!levelDB.level ? 1 : levelDB.level + 1) * 13}XP\` 남았습니다.\n
-          [여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨이 30% 부스터를 사용할 수 있어요! (준비중)`)
+          [여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨 30% 부스터를 사용할 수 있어요! (준비중)`)
           return interaction.editReply({embeds: [successEmbed]})
         }
       }
