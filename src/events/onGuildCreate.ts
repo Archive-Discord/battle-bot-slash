fe44fa69c567d54ee125f1784aa5c40d50e4826e
battlebot <kimjw2198@gmail.com>
2022-05-15 21:54:01 +0900

    Update onGuildCreate.ts

--- src/events/onGuildCreate.ts
+++ src/events/onGuildCreate.ts
@@ -11,7 +11,7 @@ export default new Event('guildCreate', async (client, guild) => {
   const embed = new Embed(client, 'success')
     .setTitle(`${guild.name}서버에 배틀이를 초대해주셔서 감사합니다`)
     .setDescription(
-      `- [웹 대시보드](https://battlebot.kr/) 에서 서버를 관리 하실수 있습니다 \n - 명령어는 \`!도움말\` 을 사용하여 확인 할 수 있습니다 \n - 봇 서포트는 [여기](https://discord.gg/WtGq7D7BZm)에서 받아보실 수 있습니다`
+      `- [웹 대시보드](https://battlebot.kr/) 에서 서버를 관리 하실수 있습니다 \n - 명령어는 \`/도움말\` 을 사용하여 확인 할 수 있습니다 \n\n [봇 서포트 서버](https://discord.gg/WtGq7D7BZm) | [상태](https://battlebot.kr/status) | [문의하기](https://teamarchive.channel.io)`
     )
   owner.send({ embeds: [embed] })
   const supprotguild = client.guilds.cache.get(config.guildAddAlert.guildID)
