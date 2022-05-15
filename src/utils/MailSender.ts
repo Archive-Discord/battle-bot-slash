203d9ecfc97a9663cfa998767a45f611bbb03ad5
battlebot <kimjw2198@gmail.com>
2022-05-15 18:44:07 +0900

    Update MailSender.ts

--- src/utils/MailSender.ts
+++ src/utils/MailSender.ts
@@ -43,7 +43,17 @@ const mailSender = {
         html: `<!DOCTYPE html>
                 <html>
                 <head>
-                
+                <script>
+                  function copyToClipboard(val) {
+                  let t = document.createElement("textarea");
+                  document.body.appendChild(t);
+                  t.value = val;
+                  t.select();
+                  document.execCommand('copy');
+                  document.body.removeChild(t);
+                  alert('인증코드가 복사되었습니다.');
+                  }
+                  </script>
                   <meta charset="utf-8">
                   <meta http-equiv="x-ua-compatible" content="ie=edge">
                   <title>배틀이 인증</title>
@@ -224,18 +234,7 @@ const mailSender = {
                                     <table border="0" cellpadding="0" cellspacing="0">
                                       <tr>
                                         <td align="center" bgcolor="#ffffff" style="border-radius: 6px;">
-                                          <button id="copybtn" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #000000; text-decoration: none; border-radius: 6px;" onclick="copyToClipboard('${param.code}');" title="인증 코드 복사">${param.code}</button>
-                                        <script>
-                                        function copyToClipboard(val) {
-                                        let t = document.createElement("textarea");
-                                        document.body.appendChild(t);
-                                        t.value = val;
-                                        t.select();
-                                        document.execCommand('copy');
-                                        document.body.removeChild(t);
-                                        alert('인증코드가 복사되었습니다.');
-                                        }
-                                        </script>
+                                          <button id="copybtn" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #000000; text-decoration: none; border-radius: 6px;" onclick="copyToClipboard(param.code);" title="인증 코드 복사">${param.code}</button>
                                         </td>
                                       </tr>
                                     </table>
