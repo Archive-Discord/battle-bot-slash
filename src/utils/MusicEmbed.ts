c403dbcd6f16b6b9e1d47704d639221c6aee4258
battlebot <kimjw2198@gmail.com>
2022-05-19 20:16:21 +0900

    Update MusicEmbed.ts

--- src/utils/MusicEmbed.ts
+++ src/utils/MusicEmbed.ts
@@ -20,7 +20,7 @@ export default class Embed extends MessageEmbed {
         iconURL: 'https://cdn.discordapp.com/emojis/667750713698549781.gif?v=1'
       }
       EmbedJSON.title = '❌ 노래가 재생 중이지 않아요!\n 해당 채널에 노래 제목을 입력해주세요!'
-      EmbedJSON.description = `[대시보드](${config.web?.baseurl}) | [서포트 서버](https://discord.gg/WtGq7D7BZm)`
+      EmbedJSON.description = `[대시보드](${config.web?.baseurl}) | [서포트 서버](https://discord.gg/WtGq7D7BZm) | [상태](https://battlebot.kr/status)`
       EmbedJSON.image = {
         url: 'https://cdn.discordapp.com/attachments/901745892418256910/941301364095586354/46144c4d9e1cf2e6.png'
       }
@@ -30,7 +30,7 @@ export default class Embed extends MessageEmbed {
         iconURL: 'https://cdn.discordapp.com/emojis/667750713698549781.gif?v=1',
         url: track.url
       }
-      EmbedJSON.description = `[대시보드](${config.web?.baseurl}) | [서포트 서버](https://discord.gg/WtGq7D7BZm)`
+      EmbedJSON.description = `[대시보드](${config.web?.baseurl}) | [서포트 서버](https://discord.gg/WtGq7D7BZm) | [상태](https://battlebot.kr/status)`
       EmbedJSON.title = `${track.title} - ${track.author} (${track.duration})`
       EmbedJSON.image = {
         url: 'https://cdn.discordapp.com/attachments/901745892418256910/941301364095586354/46144c4d9e1cf2e6.png'
