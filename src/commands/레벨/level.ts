9a07baa91c17c228f2e4a60147701e159010e476
battlebot <kimjw2198@gmail.com>
2022-05-21 15:59:29 +0900

    Update level.ts

--- src/commands/레벨/level.ts
+++ src/commands/레벨/level.ts
@@ -44,11 +44,13 @@ export default new BaseCommand(
         const levelDB = await Level.findOne({guild_id: interaction.guild.id, user_id: interaction.user.id})
         if(!levelDB) {
           successEmbed.setTitle(`${interaction.user.username}님의 레벨 정보`)
+          successEmbed.setColor('#2f3136')
           successEmbed.setDescription(`현재 \`LV.0\`입니다. 다음 레벨까지 \`0XP / 15XP\` 남았습니다.\n
           ${isPremium? "**배틀이 프리미엄으로 30% 경험치 부스터가 적용되었어요**" : `**[여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨 30% 부스터를 사용할 수 있어요!**`}`)
           return interaction.editReply({embeds: [successEmbed]})
         } else {
           successEmbed.setTitle(`${interaction.user.username}님의 레벨 정보`)
+          successEmbed.setColor('#2f3136')
           successEmbed.setDescription(`현재 \`LV.${levelDB.level ? levelDB.level : 0}\`입니다. 다음 레벨까지 \`${levelDB.currentXP.toFixed(1)}XP / ${(!levelDB.level ? 1 : levelDB.level + 1) * 13}XP\` 남았습니다.\n
           ${isPremium? "**배틀이 프리미엄으로 30% 경험치 부스터가 적용되었어요**" : `**[여기](${config.web.baseurl}/premium)에서 프리미엄을 사용하시면 레벨 30% 부스터를 사용할 수 있어요!**`}`)
           return interaction.editReply({embeds: [successEmbed]})
@@ -57,10 +59,12 @@ export default new BaseCommand(
         const levelDB = await Level.findOne({guild_id: interaction.guild.id, user_id: user.id})
         if(!levelDB) {
           successEmbed.setTitle(`${user.username}님의 레벨 정보`)
+          successEmbed.setColor('#2f3136')
           successEmbed.setDescription(`현재 \`LV.0\`입니다. 다음 레벨까지 \`0XP / 15XP\` 남았습니다.`)
           return interaction.editReply({embeds: [successEmbed]})
         } else {
           successEmbed.setTitle(`${user.username}님의 레벨 정보`)
+          successEmbed.setColor('#2f3136')
           successEmbed.setDescription(`현재 \`LV.${levelDB.level ? levelDB.level : 0}\`입니다. 다음 레벨까지 \`${levelDB.currentXP.toFixed(1)}XP / ${(!levelDB.level ? 1 : levelDB.level + 1) * 13}XP\` 남았습니다.`)
           return interaction.editReply({embeds: [successEmbed]})
         }
