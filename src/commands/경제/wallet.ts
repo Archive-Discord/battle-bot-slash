9c75e874c63f1d5d4a8961a7c766c454981ce029
이루 <100296449+erukim@users.noreply.github.com>
2022-07-07 10:40:40 +0900

    빗금 기능 추가

--- src/commands/경제/wallet.ts
+++ src/commands/경제/wallet.ts
@@ -1,8 +1,9 @@
-import { BaseCommand } from '../../structures/Command'
+import { BaseCommand, SlashCommand } from '../../structures/Command'
 import Discord from 'discord.js'
 import Embed from '../../utils/Embed'
 import comma from 'comma-number'
 import Schema from '../../schemas/Money'
+import { SlashCommandBuilder, userMention } from '@discordjs/builders'
 
 export default new BaseCommand(
   {
@@ -39,5 +40,51 @@ export default new BaseCommand(
     m.edit({
       embeds: [embed]
     })
+  },
+  {
+    // @ts-ignore
+    data: new SlashCommandBuilder()
+      .setName('돈')
+      .setDescription('자신의 돈을 확인합니다.')
+      .addUserOption(option =>
+      option
+        .setName("유저")
+        .setDescription("확인할 유저를 입력해주세요")
+        .setRequired(false)
+    ),
+    options: {
+      name: '돈',
+      isSlash: true
+    },
+    async execute(client, interaction) {
+      await interaction.deferReply({ })
+      let embed = new Embed(client, 'warn')
+      .setTitle('처리중..')
+      .setColor('#2f3136')
+    let m = await interaction.editReply({
+      embeds: [embed]
+    })
+      let user = interaction.options.getUser('유저') || interaction.user
+      const wjdqh = await Schema.findOne({ userid: user.id })
+      embed = new Embed(client, 'success').setTitle(`정보 오류`)
+        .setDescription(`아쉽지만 ${interaction.user}님의 정보가 기록되어있지 않습니다.\n!돈받기 명령어로 정보를 알려주세요.`)
+
+      if (!wjdqh) return interaction.editReply({
+        embeds: [embed]
+      })
+        const t = new Date()
+        const date = "" + t.getFullYear() + t.getMonth() + t.getDate();
+        let i
+        if (wjdqh.date == date) i = "돈을 받음"
+        else i = "돈을 받지않음"
+        embed = new Embed(client, 'success')
+          .setTitle(`${user.tag}님의 잔액`)
+          .setDescription(`유저님의 잔액은 아래와 같습니다.`)
+          .addField("잔액 :", `**${comma(wjdqh.money)}원**`)
+          .setColor('#2f3136')
+        interaction.editReply({
+          embeds: [embed]
+        })
+    }
   }
 )
