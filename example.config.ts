c7edb891690261e75f79ac1dfa286ff8c8cb8be8
battlebotdev <kimjw2198@gmail.com>
2022-07-29 23:10:47 +0900

    Edit

--- example.config.ts
+++ example.config.ts
@@ -22,15 +22,17 @@ if (BUILD_NUMBER?.indexOf(':') === -1) {
 let config: IConfig = {
   BUILD_NUMBER,
   BUILD_VERSION: '0.0.1-dev',
-  githubToken: '',
-  pubgapikey: '',
-  klaytnapikey: '',
+  githubToken: '1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',
+  pubgapikey:
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiYzc5OTFlMC1kYmQxLTAxMzktZGYwMi0wYWRjMjYyMjExNmMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjI4NTc2NzUyLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Ii0yYmFmNzQ4NC0zYWVkLTQ1MDAtODFhOC1kNDA4MmM3NTE4NmEifQ.XQSrzmKpJ09F4OCS_LtEwO4bCGwK8-pp3N5IHMcNjss',
+  klaytnapikey:
+    'S0FTS0c4QTk0SExGQjFLMzczT1k3RkZKOlNMeWNkNG5VVlJxaEZnMUp4cHBsYVVLTWZDQkRBN0lIQzFld3dQOEU=',
   updateServer: {
     koreanbots: '', // https://koreanbots.dev
     archive: '' // https://archiver.me
   },
   web: {
-    baseurl: ''
+    baseurl: 'http://localhost/'
   },
   bot: {
     sharding: false,
@@ -38,8 +40,9 @@ let config: IConfig = {
       intents: [130815],
       allowedMentions: { parse: ['users', 'roles'], repliedUser: false }
     },
-    token: '',
-    owners: [],
+    token:
+      'OTI4MjU0NTA5NjUyNzcwODU2.GZpJW6.dBXHmy42fj0gihPhJ4eHoG0XvF6qA7oKIxTtGE',
+    owners: ['896570484588703744'],
     prefix: '!',
     cooldown: 2000
   },
@@ -52,17 +55,17 @@ let config: IConfig = {
       url: ''
     },
     text: {
-      guildID: '',
-      channelID: ''
+      guildID: '916704781551169577',
+      channelID: '1001488570470641735'
     }
   },
   guildAddAlert: {
-    guildID: '',
-    channelID: ''
+    guildID: '916704781551169577',
+    channelID: '1001488570470641735'
   },
   devGuild: {
-    guildID: '',
-    channelID: ''
+    guildID: '916704781551169577',
+    channelID: '1001488570470641735'
   },
   database: {
     /**
@@ -80,11 +83,13 @@ let config: IConfig = {
     dev: false
   },
   email: {
-    Google_Email: '',
-    Google_Client_Id: '',
-    Google_Client_Secret: '',
-    Google_Redirect_Url: '',
-    Google_Refresh_Token: ''
+    Google_Email: 'admin@battlebot.kr',
+    Google_Client_Id:
+      '481289387359-clq605bfcm5nmbhf9h9k52aqta2s67ss.apps.googleusercontent.com',
+    Google_Client_Secret: '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',
+    Google_Redirect_Url: 'https://developers.google.com/oauthplayground',
+    Google_Refresh_Token:
+      '1//0496BCJG9FetdCgYIARAAGAQSNwF-L9IrPD9dps2dMjQ7uuvqunrUrGQMnNK_Tn37GI7c17bdvwud1OMZHiORDk6vQtiRSYknsPA'
   },
   i18n: {
     options: {
