531b5117b783af010945bf708b2182730eca6353
battlebotdev <kimjw2198@gmail.com>
2022-07-30 23:44:11 +0900

    Edit

--- src/commands/경제/gamble.ts
+++ src/commands/경제/gamble.ts
@@ -12,10 +12,9 @@ export default new BaseCommand(
     aliases: ['도박', 'ehqkr']
   },
   async (client, message, args) => {
-    let embed = new Embed(client, 'warn').setTitle(
-      client.i18n.t('commands.gamble.loading')
-    )
-
+    let embed = new Embed(client, 'warn')
+      .setTitle(client.i18n.t('commands.gamble.loading'))
+      .setColor('#2f3136')
     let m = await message.reply({
       embeds: [embed]
     })
@@ -24,7 +23,9 @@ export default new BaseCommand(
     })
     embed = new Embed(client, 'error')
       .setTitle(client.i18n.t('main.error.title'))
-      .setDescription(client.i18n.t('commands.gamble.error.account'))
+      .setDescription(
+        client.i18n.t('commands.gamble.error.description.account')
+      )
       .setTimestamp()
       .setColor('#2f3136')
     if (!ehqkrduqn)
@@ -33,7 +34,9 @@ export default new BaseCommand(
       })
     embed = new Embed(client, 'error')
       .setTitle(client.i18n.t('main.error.title'))
-      .setDescription(client.i18n.t('commands.gamble.error.scanmoney'))
+      .setDescription(
+        client.i18n.t('commands.gamble.error.description.scanmoney')
+      )
       .setTimestamp()
       .setColor('#2f3136')
     if (!args[0])
@@ -42,7 +45,7 @@ export default new BaseCommand(
       })
     embed = new Embed(client, 'error')
       .setTitle(client.i18n.t('main.error.title'))
-      .setDescription(client.i18n.t('commands.gamble.error.int'))
+      .setDescription(client.i18n.t('commands.gamble.error.description.int'))
       .setTimestamp()
       .setColor('#2f3136')
     if (args.join(' ').includes('-'))
@@ -52,7 +55,7 @@ export default new BaseCommand(
     const money = parseInt(args[0])
     embed = new Embed(client, 'error')
       .setTitle(client.i18n.t('main.error.title'))
-      .setDescription(client.i18n.t('commands.gamble.error.toolow'))
+      .setDescription(client.i18n.t('commands.gamble.error.description.toolow'))
       .setTimestamp()
       .setColor('#2f3136')
     if (money < 1000)
@@ -60,9 +63,9 @@ export default new BaseCommand(
         embeds: [embed]
       })
     embed = new Embed(client, 'error')
-      .setTitle(`❌ 에러 발생`)
+      .setTitle(client.i18n.t('main.error.title'))
       .setDescription(
-        '보유하신 돈보다 배팅하신 돈의 금액이 많습니다. 금액 확인 부탁드립니다.'
+        client.i18n.t('commands.gamble.error.description.toomany')
       )
       .setTimestamp()
       .setColor('#2f3136')
@@ -73,15 +76,17 @@ export default new BaseCommand(
     const random = Math.floor(Math.random() * 101)
     if (random < 30) {
       embed = new Embed(client, 'success')
-        .setTitle(client.i18n.t('main.error.title'))
+        .setTitle(client.i18n.t('commands.gamble.success.title.fail'))
         .setDescription(
           client.i18n.t('commands.gamble.success.description.fail', {
             money: comma(money)
           })
         )
         .addFields({
-          name: '잔액 :',
-          value: `**${comma(ehqkrduqn.money - money)}원**`
+          name: client.i18n.t('commands.gamble.success.fields.name'),
+          value: client.i18n.t('commands.gamble.success.fields.value', {
+            fmoney: comma(ehqkrduqn.money - money)
+          })
         })
         .setColor('#2f3136')
       m.edit({
@@ -97,15 +102,17 @@ export default new BaseCommand(
       )
     } else {
       embed = new Embed(client, 'success')
-        .setTitle(`⭕ 도박 성공`)
+        .setTitle(client.i18n.t('commands.gamble.success.title.success'))
         .setDescription(
           client.i18n.t('commands.gamble.success.description.success', {
             money: comma(money)
           })
         )
         .addFields({
-          name: '잔액 :',
-          value: `**${comma(ehqkrduqn.money + money)}원**`
+          name: client.i18n.t('commands.gamble.success.fields.name'),
+          value: client.i18n.t('commands.gamble.success.fields.value', {
+            fmoney: comma(ehqkrduqn.money + money)
+          })
         })
         .setColor('#2f3136')
       m.edit({
@@ -138,10 +145,8 @@ export default new BaseCommand(
         userid: interaction.user.id
       })
       let embed = new Embed(client, 'error')
-        .setTitle(`❌ 에러 발생`)
-        .setDescription(
-          '계좌가 생성되어있지 않습니다. !돈받기 입력 부탁드립니다.'
-        )
+        .setTitle(client.i18n.t('main.error.title'))
+        .setDescription(client.i18n.t('commands.gamble.error.account'))
         .setTimestamp()
         .setColor('#2f3136')
       if (!ehqkrduqn)
@@ -150,8 +155,10 @@ export default new BaseCommand(
         })
       let money = interaction.options.getInteger('베팅금') || 0 //parseInt();
       embed = new Embed(client, 'error')
-        .setTitle(`❌ 에러 발생`)
-        .setDescription('1000원 이상부터 도박이 가능합니다.')
+        .setTitle(client.i18n.t('main.error.title'))
+        .setDescription(
+          client.i18n.t('commands.gamble.error.description.toolow')
+        )
         .setTimestamp()
         .setColor('#2f3136')
       if (money < 1000)
@@ -159,8 +166,10 @@ export default new BaseCommand(
           embeds: [embed]
         })
       embed = new Embed(client, 'error')
-        .setTitle(`❌ 에러 발생`)
-        .setDescription('배팅하신 금액이 보유하신 금액보다 큽니다.')
+        .setTitle(client.i18n.t('main.error.title'))
+        .setDescription(
+          client.i18n.t('commands.gamble.error.description.toomany')
+        )
         .setTimestamp()
         .setColor('#2f3136')
       if (money > ehqkrduqn.money)
@@ -170,15 +179,17 @@ export default new BaseCommand(
       const random = Math.floor(Math.random() * 101)
       if (random < 50) {
         embed = new Embed(client, 'success')
-          .setTitle(`❌ 도박 실패`)
+          .setTitle(client.i18n.t('commands.gamble.success.title.fail'))
           .setDescription(
-            `도박에 실패하셨습니다. 돈은 제가 가져가겠습니다. - **${comma(
-              money
-            )}원**`
+            client.i18n.t('commands.gamble.success.description.fail', {
+              money: comma(money)
+            })
           )
           .addFields({
-            name: '잔액 :',
-            value: `**${comma(ehqkrduqn.money - money)}원**`
+            name: client.i18n.t('commands.gamble.success.fields.name'),
+            value: client.i18n.t('commands.gamble.success.fields.value', {
+              fmoney: comma(ehqkrduqn.money - money)
+            })
           })
           .setColor('#2f3136')
         interaction.editReply({
@@ -194,11 +205,17 @@ export default new BaseCommand(
         )
       } else {
         embed = new Embed(client, 'success')
-          .setTitle(`⭕ 도박 성공`)
-          .setDescription(`도박에 성공하셨습니다. + **${comma(money)}원**`)
+          .setTitle(client.i18n.t('commands.gamble.success.title.success'))
+          .setDescription(
+            client.i18n.t('commands.gamble.success.description.success', {
+              money: comma(money)
+            })
+          )
           .addFields({
-            name: '잔액 :',
-            value: `**${comma(ehqkrduqn.money + money)}원**`
+            name: client.i18n.t('commands.gamble.success.fields.name'),
+            value: client.i18n.t('commands.gamble.success.fields.value', {
+              fmoney: comma(ehqkrduqn.money + money)
+            })
           })
           .setColor('#2f3136')
         interaction.editReply({
