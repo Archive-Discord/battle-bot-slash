e1dad59eaf63e3a30c6d487388a0b538a1a93100
battlebotdev <admin@battlebot.kr>
2023-02-12 13:14:19 +0900

    코드 안정화

--- src/commands/경제/sendMoney.ts
+++ src/commands/경제/sendMoney.ts
@@ -12,89 +12,67 @@ export default new BaseCommand(
     aliases: ['송금', 'moneysay', 'thdrma'],
   },
   async (client, message, args) => {
-    let embed = new Embed(client, 'warn').setTitle('생각하는 중...');
-    let m = await message.reply({
-      embeds: [embed],
-    });
     const user = message.mentions.users.first();
-    embed = new Embed(client, 'error')
-      .setTitle(`❌ 에러 발생`)
-      .setDescription(`송금할 대상이 지정되지 않았습니다.`)
-      .setColor('#2f3136');
-    if (!user)
-      return m.edit({
-        embeds: [embed],
-      });
     const sk = await Schema.findOne({ userid: message.author.id });
+    if (!user) {
+      let embed = new Embed(client, 'error')
+        .setTitle(`❌ 에러 발생`)
+        .setDescription(`송금할 대상이 지정되지 않았습니다.`)
+      return message.reply({ embeds: [embed] });
+    }
     const tkdeoqkd = await Schema.findOne({ userid: user.id });
-    embed = new Embed(client, 'error')
-      .setDescription(
-        message.author +
-        '님의 계좌가 생성되지 않으셨습니다. \n계좌가 있으신 유저에게만 송금이 가능합니다.',
-      )
-      .setTimestamp()
-      .setColor('#2f3136');
-    if (!sk)
-      return m.edit({
-        embeds: [embed],
-      });
-    embed = new Embed(client, 'error')
-      .setDescription('계좌가 생성되지 않으셨습니다. \n계좌가 있으신 유저에게만 송금이 가능합니다.')
-      .setTimestamp()
-      .setColor('#2f3136');
-    if (!tkdeoqkd)
-      return m.edit({
-        embeds: [embed],
-      });
+    if (!sk) {
+      let embed = new Embed(client, 'error')
+        .setTitle(`❌ 에러 발생`)
+        .setDescription(message.author.toString() + '님의 계좌가 생성되지 않으셨습니다. \n계좌가 있으신 유저에게만 송금이 가능합니다.')
+        .setTimestamp()
+      return message.reply({ embeds: [embed] });
+    }
+    else if (!tkdeoqkd) {
+      let embed = new Embed(client, 'error')
+        .setTitle(`❌ 에러 발생`)
+        .setDescription('계좌가 생성되지 않으셨습니다. \n계좌가 있으신 유저에게만 송금이 가능합니다.')
+        .setTimestamp()
+      return message.reply({ embeds: [embed] });
+    }
     const betting = parseInt(args[1]);
-    embed = new Embed(client, 'error')
-      .setTitle(`❌ 에러 발생`)
-      .setDescription('사용법 : !송금 @멘션 (금액)')
-      .setTimestamp()
-      .setColor('#2f3136');
-    if (!betting)
-      return m.edit({
-        embeds: [embed],
-      });
-
-    embed = new Embed(client, 'error')
-      .setTitle(`❌ 에러 발생`)
-      .setDescription('금액은 자연수만 입력해주세요.')
-      .setTimestamp()
-      .setColor('#2f3136');
-    if (message.content.includes('-'))
-      return m.edit({
-        embeds: [embed],
-      });
-    embed = new Embed(client, 'error')
-      .setTitle(`❌ 에러 발생`)
-      .setDescription('1')
-      .setTimestamp()
-      .setColor('#2f3136');
-    if (betting < 1000)
-      return m.edit({
-        embeds: [embed],
-      });
     const money = parseInt(String(sk.money));
     const money2 = parseInt(String(tkdeoqkd.money));
-    embed = new Embed(client, 'error')
-      .setTitle(`❌ 에러 발생`)
-      .setDescription('보내실려는 금액이 보유하신 금액보다 큽니다.')
-      .setTimestamp()
-      .setColor('#2f3136');
-    if (money < betting)
-      return m.edit({
-        embeds: [embed],
-      });
-    embed = new Embed(client, 'error')
-      .setTitle(`❌ 에러 발생`)
-      .setDescription('돈을 본인에게 보내실 수 없습니다.')
-      .setTimestamp()
-      .setColor('#2f3136');
-    if (message.author.id == user.id)
-      return m.edit({
-        embeds: [embed],
-      });
+    if (!betting) {
+      let embed = new Embed(client, 'error')
+        .setTitle(`❌ 에러 발생`)
+        .setDescription('사용법 : !송금 @멘션 (금액)')
+        .setTimestamp()
+      return message.reply({ embeds: [embed] });
+    }
+    else if (message.content.includes('-')) {
+      let embed = new Embed(client, 'error')
+        .setTitle(`❌ 에러 발생`)
+        .setDescription('금액은 자연수만 입력해주세요.')
+        .setTimestamp()
+      return message.reply({ embeds: [embed] });
+    }
+    else if (betting < 1000) {
+      let embed = new Embed(client, 'error')
+        .setTitle(`❌ 에러 발생`)
+        .setDescription('최소 배팅 금액은 1,000원 입니다. 1,000원 이상으로 입력해주세요.')
+        .setTimestamp()
+      return message.reply({ embeds: [embed] });
+    }
+    else if (money < betting) {
+      let embed = new Embed(client, 'error')
+        .setTitle(`❌ 에러 발생`)
+        .setDescription('보내실려는 금액이 보유하신 금액보다 큽니다.')
+        .setTimestamp()
+      return message.reply({ embeds: [embed] });
+    }
+    else if (message.author.id == user.id) {
+      let embed = new Embed(client, 'error')
+        .setTitle(`❌ 에러 발생`)
+        .setDescription('돈을 본인에게 보내실 수 없습니다.')
+        .setTimestamp()
+      return message.reply({ embeds: [embed], });
+    }
     await Schema.findOneAndUpdate(
       { userid: message.author.id },
       {
@@ -113,21 +91,22 @@ export default new BaseCommand(
         date: tkdeoqkd.date,
       },
     );
-    embed = new Embed(client, 'info')
+    let embed = new Embed(client, 'default')
       .setTitle('⭕ 송금 완료')
       .addFields(
         {
           name: `송금인 잔액`,
           value: `${comma(money - betting)}원`,
-          inline: true,
+          inline: true
         },
-        { name: `받는사람 잔액`, value: ` ${money2 + betting}원`, inline: true },
+        {
+          name: `받는사람 잔액`,
+          value: ` ${money2 + betting}원`,
+          inline: true
+        },
       )
       .setTimestamp()
-      .setColor('#2f3136');
-    m.edit({
-      embeds: [embed],
-    });
+    message.reply({ embeds: [embed] });
   },
   {
     // @ts-ignore
@@ -152,66 +131,51 @@ export default new BaseCommand(
       let user = interaction.options.getMember('유저') || interaction.member;
       let user2 = interaction.options.getUser('유저') || interaction.user;
       let betting = interaction.options.getInteger('송금액') || 0;
-      let embed = new Embed(client, 'error')
-        .setTitle(`❌ 에러 발생`)
-        .setDescription(`송금할 대상이 지정되지 않았습니다.`)
-        .setColor('#2f3136');
-      if (!user)
-        return interaction.editReply({
-          embeds: [embed],
-        });
+      if (!user) {
+        let embed = new Embed(client, 'error')
+          .setTitle(`❌ 에러 발생`)
+          .setDescription(`송금할 대상이 지정되지 않았습니다.`)
+        return interaction.editReply({ embeds: [embed] });
+      }
       const sk = await Schema.findOne({ userid: interaction.user.id });
       const tkdeoqkd = await Schema.findOne({ userid: user2.id });
-      embed = new Embed(client, 'error')
-        .setDescription(
-          interaction.user +
-          '님의 계좌가 생성되지 않으셨습니다.\n계좌가 있으신 유저에게만 송금이 가능합니다.',
-        )
-        .setTimestamp()
-        .setColor('#2f3136');
-      if (!sk)
-        return interaction.editReply({
-          embeds: [embed],
-        });
-      embed = new Embed(client, 'error')
-        .setDescription(
-          '계좌가 생성되지 않으셨습니다. \n계좌가 있으신 유저에게만 송금이 가능합니다.',
-        )
-        .setTimestamp()
-        .setColor('#2f3136');
-      if (!tkdeoqkd)
-        return interaction.editReply({
-          embeds: [embed],
-        });
-      embed = new Embed(client, 'error')
-        .setTitle(`❌ 에러 발생`)
-        .setDescription('1000원 이상부터 송금이 가능합니다.')
-        .setTimestamp()
-        .setColor('#2f3136');
-      if (betting < 1000)
-        return interaction.editReply({
-          embeds: [embed],
-        });
+      if (!sk) {
+        let embed = new Embed(client, 'error')
+          .setTitle(`❌ 에러 발생`)
+          .setDescription(interaction.user.toString() + '님의 계좌가 생성되지 않으셨습니다.\n계좌가 있으신 유저에게만 송금이 가능합니다.')
+          .setTimestamp()
+        return interaction.editReply({ embeds: [embed] });
+      }
+      else if (!tkdeoqkd) {
+        let embed = new Embed(client, 'error')
+          .setTitle(`❌ 에러 발생`)
+          .setDescription('계좌가 생성되지 않으셨습니다. \n계좌가 있으신 유저에게만 송금이 가능합니다.',)
+          .setTimestamp()
+        return interaction.editReply({ embeds: [embed] });
+      }
       const money = parseInt(String(sk.money));
       const money2 = parseInt(String(tkdeoqkd.money));
-      embed = new Embed(client, 'error')
-        .setTitle(`❌ 에러 발생`)
-        .setDescription('보내실려는 금액이 보유하신 금액보다 큽니다.')
-        .setTimestamp()
-        .setColor('#2f3136');
-      if (money < betting)
-        return interaction.editReply({
-          embeds: [embed],
-        });
-      embed = new Embed(client, 'error')
-        .setTitle(`❌ 에러 발생`)
-        .setDescription('돈을 본인에게 보내실 수 없습니다.')
-        .setTimestamp()
-        .setColor('#2f3136');
-      if (interaction.user.id == user2.id)
-        return interaction.editReply({
-          embeds: [embed],
-        });
+      if (betting < 1000) {
+        let embed = new Embed(client, 'error')
+          .setTitle(`❌ 에러 발생`)
+          .setDescription('1000원 이상부터 송금이 가능합니다.')
+          .setTimestamp()
+        return interaction.editReply({ embeds: [embed] });
+      }
+      else if (money < betting) {
+        let embed = new Embed(client, 'error')
+          .setTitle(`❌ 에러 발생`)
+          .setDescription('보내실려는 금액이 보유하신 금액보다 큽니다.')
+          .setTimestamp()
+        return interaction.editReply({ embeds: [embed] });
+      }
+      else if (interaction.user.id == user2.id) {
+        let embed = new Embed(client, 'error')
+          .setTitle(`❌ 에러 발생`)
+          .setDescription('돈을 본인에게 보내실 수 없습니다.')
+          .setTimestamp()
+        return interaction.editReply({ embeds: [embed] });
+      }
       await Schema.findOneAndUpdate(
         { userid: interaction.user.id },
         {
@@ -230,7 +194,7 @@ export default new BaseCommand(
           date: tkdeoqkd.date,
         },
       );
-      embed = new Embed(client, 'info')
+      let embed = new Embed(client, 'default')
         .setTitle('⭕ 송금 완료')
         .addFields(
           {
@@ -240,15 +204,12 @@ export default new BaseCommand(
           },
           {
             name: `받는사람 잔액`,
-            value: ` ${money2 + betting}원`,
+            value: `${money2 + betting}원`,
             inline: true,
           },
         )
         .setTimestamp()
-        .setColor('#2f3136');
-      interaction.editReply({
-        embeds: [embed],
-      });
+      interaction.editReply({ embeds: [embed] });
     },
   },
 );
