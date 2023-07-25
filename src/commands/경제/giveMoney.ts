9488c90cfa66a5a5431449eb7988fcc08571177c
kiss8981 <kdh89811@gmail.com>
2023-07-26 01:10:38 +0900

    fix: give moneny

--- src/commands/경제/giveMoney.ts
+++ src/commands/경제/giveMoney.ts
@@ -7,6 +7,7 @@ import Attendance from '../../schemas/attendanceSchema';
 import DateFormatting from '../../utils/DateFormatting';
 import dayjs from 'dayjs';
 import config from '../../../config';
+import Money from '../../schemas/Money';
 
 export default new BaseCommand(
   {
@@ -16,20 +17,31 @@ export default new BaseCommand(
   },
   async (client, message, args) => {
     const attendances = await Attendance.findOne({ user_id: message.author.id, date: { $gte: new Date(new Date().setHours(0, 0, 0, 0)) } });
+    let money = await Money.findOne({ userid: message.author.id });
     if (!attendances) {
-      let embed = new Embed(client, 'info')
-        .setTitle('⭕ 출석 가능')
-        .setDescription(`아직 오늘 출석하지 않았네요! [여기](${config.web.baseurl}/calendar)를 들어가서 출석을 해주세요!`)
+      if (!money) {
+        money = await Money.create({
+          userid: message.author.id,
+          money: 0,
+        });
+      }
+      await Attendance.create({
+        user_id: message.author.id,
+        date: new Date(),
+      });
+      await money.updateOne({ $inc: { money: 20000 } });
+      const embed = new Embed(client, 'default')
+        .setTitle('⭕ 출석완료')
+        .setDescription(`출석이 완료되었습니다! 출석 보상으로 20000원을 받았습니다!`)
       return message.reply({ embeds: [embed] });
-    }
-    else {
+    } else {
       let embed = new Embed(client, 'error')
         .setTitle('❌ 에러 발생')
         .setDescription(`이미 오늘은 출석을 하셨어요 ${DateFormatting._format(dayjs(dayjs().add(1, "day").toDate()).set('hour', 0).set('minute', 0).toDate(), 'R')}에 다시 와주세요!`)
         .addFields([
           {
             name: `하트인증`,
-            value: `돈을 더 얻고 싶으시다면 '!하트인증'으로 돈을 더 얻으실 수 있습니다!`,
+            value: `돈을 더 얻고 싶으시다면 \`!하트인증\`으로 돈을 더 얻으실 수 있습니다!`,
             inline: true
           }
         ]);
@@ -37,7 +49,6 @@ export default new BaseCommand(
     }
   },
   {
-    // @ts-ignore
     data: new SlashCommandBuilder()
       .setName('돈받기')
       .setDescription('출석체크를 합니다.'),
@@ -48,20 +59,31 @@ export default new BaseCommand(
     async execute(client, interaction) {
       await interaction.deferReply({ ephemeral: true });
       const attendances = await Attendance.findOne({ user_id: interaction.user.id, date: { $gte: new Date(new Date().setHours(0, 0, 0, 0)) } });
+      let money = await Money.findOne({ userid: interaction.user.id });
       if (!attendances) {
-        let embed = new Embed(client, 'default')
-          .setTitle('⭕ 출석 가능')
-          .setDescription(`아직 오늘 출석하지 않았네요! [여기](${config.web.baseurl}/calendar)를 들어가서 출석을 해주세요!`)
+        if (!money) {
+          money = await Money.create({
+            userid: interaction.user.id,
+            money: 0,
+          });
+        }
+        await Attendance.create({
+          user_id: interaction.user.id,
+          date: new Date(),
+        });
+        await money.updateOne({ $inc: { money: 20000 } });
+        const embed = new Embed(client, 'default')
+          .setTitle('⭕ 출석완료')
+          .setDescription(`출석이 완료되었습니다! 출석 보상으로 20000원을 받았습니다!`)
         return interaction.editReply({ embeds: [embed] });
-      }
-      else {
-        let embed = new Embed(client, 'error')
+      } else {
+        const embed = new Embed(client, 'error')
           .setTitle('❌ 에러 발생')
           .setDescription(`이미 오늘은 출석을 하셨어요 ${DateFormatting._format(dayjs(dayjs().add(1, "day").toDate()).set('hour', 0).set('minute', 0).toDate(), 'R')}에 다시 와주세요!`)
           .addFields([
             {
               name: `하트인증`,
-              value: `돈을 더 얻고 싶으시다면 '!하트인증'으로 돈을 더 얻으실 수 있습니다!`,
+              value: `돈을 더 얻고 싶으시다면 \`!하트인증\`으로 돈을 더 얻으실 수 있습니다!`,
               inline: true
             }
           ]);
